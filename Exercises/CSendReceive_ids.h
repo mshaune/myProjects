bcb282502ba0dcbb98ee2c9d1844a5f12cddbfac
mshaune <38355310+mshaune@users.noreply.github.com>
2018-04-17 19:07:10 -0500

    Add files via upload

---
+++ Exercises/CSendReceive_ids.h
@@ -0,0 +1,33 @@
+/*
+* CSCI 290 - Event Driver Programming
+* Chuck Nelson, Associate Professor
+*
+* Student Information (please complete ? below)
+*    Name:         Myracle Jackson
+*    School Email: mj148182@rockford.edu
+* Class Information
+*    Section:      290.01
+* Assignment Information
+*                  Exercise4.5
+*                  CSendReceive App
+*---------------------------------------------------------------------
+*/
+// CSendReceive App
+// STUDENT CODE BEGINS
+#define IDC_STATIC     2101
+#define IDC_SOURCE    2103
+
+#define IDC_DESTINATION 2205
+#define IDC_RECEIVE         2300
+#define IDC_CLEAR        2304
+#define IDM_CLOSE 2577
+#define IDC_EXIT 2159
+#define IDC_SEND        2009
+
+#define IDOK       2308
+#define IDCANCEL 2312
+//STUDENT CODE ENDS
+
+
+
+
