06f974342115bec7b43bb116eafb301fb819d326
mshaune <38355310+mshaune@users.noreply.github.com>
2018-04-17 19:01:06 -0500

    Update menus_ids.h

--- menus_ids.h
+++ menus_ids.h
@@ -1,25 +1,3 @@
-/* DO NOT ALTER
-* CSCI 290 - Event Driver Programming
-* Chuck Nelson, Associate Professor
-*
-* Student Information (please complete ? below)
-*    Name:         Myracle Jackson
-*    School Email: mj148182@rockford.edu
-* Class Information
-*    Section:      290.01
-* Assignment Information
-*                  Exercise2.7
-*---------------------------------------------------------------------
-TODO #1: First, add these four files to an MFC project and run. There should be
-no errors. Try each option so that you know how the program operates
-before you enhance this program.
-
-TODO #2: Modify the menu program () by adding these features...
--include one more food item for each menu  (no changes to FILE menu)
--implement a new menu option to let the manager increase ALL prices
-by a one-percent surcharge each time this option is selected
-
-*/
 // Fig. 2.11:  menus_ids.h
 // define messages used by menus.cpp and menus.rc
 #define  IDM_EXIT        2000
